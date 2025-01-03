feb4b0bb05380ffb8e4d599dba35e612ff51b87d
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-01-03 15:25:04 +0530

    remove extra code

--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 const Number = () => {
-    const [Multiply, setMultiply] = useState(0);
+    const [Sum, setSum] = useState(0);
     useEffect(() => {
-        function MultiplyNumbers(num1, num2) {
-            return num1 * num2;
+        function addNumbers(num1, num2) {
+            return num1 + num2;
         }
-        let result = MultiplyNumbers(5, 15);
-        setMultiply(result);
+        let result = addNumbers(5, 15);
+        setSum(result);
     }, []);
     return (
         <div className='py-10'>
             <h2 className="text-4xl max-sm:text-lg font-bold pb-4 text-center">Multiply Two Numbers with Function</h2>
-            <p id="result" className='text-center font-medium text-3xl max-sm:text-base'> Total : {Multiply}</p>
+            <p id="result" className='text-center font-medium text-3xl max-sm:text-base'> Total : {Sum}</p>
         </div>
     );
 };
