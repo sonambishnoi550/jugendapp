1bf53aa157faf696d0c18941d5f1e338bb013389
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-01-03 15:42:14 +0530

    change color

--- src/components/FormValidation.jsx
+++ src/components/FormValidation.jsx
@@ -55,32 +55,32 @@ const FormValidation = () => {
             </div>
             <input id="imageInput" type="file" accept="image/*" style={{ display: "none" }} onChange={handleImageChange} multiple />
             <div className="flex p-4 rounded-[30px] border-lightgray border bg-white mb-5 max-sm:mb-3 relative">
-              <input className="w-full outline-none text-sm font-bold" type="text" name="category" placeholder="Kategorie" value={formData.category} onChange={handleChange} />
+              <input className="w-full outline-none text-sm font-bold placeholder:text-gray text-gray" type="text" name="category" placeholder="Kategorie" value={formData.category} onChange={handleChange} />
               <button type="button" onClick={handleDropdownToggle} className="relative z-10">
                 <img src="./assets/images/svg/kategorie.svg" alt="icon" /></button>
               {isDropdownOpen && (
                 <div className="absolute bg-white border border-gray-300 rounded-[10px] mt-2 w-[150px] z-10 right-[4%] top-[4%]">
                   <ul>
-                    <li className="p-2 cursor-pointer text-sm font-bold" onClick={() => handleOptionSelect('Food')}> Food </li>
-                    <li className="p-2 cursor-pointer text-sm font-bold" onClick={() => handleOptionSelect('Travel')} > Travel </li>
-                    <li className="p-2 cursor-pointer text-sm font-bold" onClick={() => handleOptionSelect('Movies')} > Movies</li>
+                    <li className="p-2 cursor-pointer text-sm font-bold text-gray" onClick={() => handleOptionSelect('Food')}> Food </li>
+                    <li className="p-2 cursor-pointer text-sm font-bold text-gray" onClick={() => handleOptionSelect('Travel')} > Travel </li>
+                    <li className="p-2 cursor-pointer text-sm font-bold text-gray" onClick={() => handleOptionSelect('Movies')} > Movies</li>
                   </ul>
                 </div>)}
             </div>
             <div className="sm:flex gap-5 mb-5 max-sm:mb-3">
-              <input className="py-[17.5px] pl-4 max-sm:w-full max-sm:mb-3  rounded-[30px] border-lightgray border outline-none text-sm font-bold w-[50%]"
+              <input className="py-[17.5px] pl-4 max-sm:w-full max-sm:mb-3 placeholder:text-gray text-gray  rounded-[30px] border-lightgray border outline-none text-sm font-bold w-[50%]"
                 type="text" required name="title" placeholder="Titel" value={formData.title} onChange={handleChange} />
-              <input className="py-[17.5px] pl-4 max-sm:w-full rounded-[30px] border-lightgray border outline-none text-sm font-bold w-[50%]"
+              <input className="py-[17.5px] pl-4 max-sm:w-full rounded-[30px] placeholder:text-gray text-gray border-lightgray border outline-none text-sm font-bold w-[50%]"
                 type="text" required name="date" placeholder="Datum" value={formData.date} onChange={handleChange} />
             </div>
             <div className="sm:flex gap-5 pb-5 max-sm:pb-3">
-              <input className="py-[17.5px] pl-4 max-sm:w-full max-sm:mb-3 rounded-[30px] border-lightgray border outline-none text-sm font-bold w-[50%]"
+              <input className="py-[17.5px] pl-4 max-sm:w-full max-sm:mb-3 rounded-[30px] placeholder:text-gray text-gray border-lightgray border outline-none text-sm font-bold w-[50%]"
                 type="text" required name="location" placeholder="Standort" value={formData.location} onChange={handleChange} />
-              <input className="py-[17.5px] pl-4 max-sm:w-full rounded-[30px] border-lightgray border outline-none text-sm font-bold w-[50%]"
+              <input className="py-[17.5px] pl-4 max-sm:w-full rounded-[30px] placeholder:text-gray text-gray border-lightgray border outline-none text-sm font-bold w-[50%]"
                 type="text" required name="price" placeholder="Preis" value={formData.price} onChange={handleChange} />
             </div>
             <textarea
-              className="w-full py-[55.5px] max-sm:pt-10 max-sm:pb-5 mb-5 leading-6 text-sm font-bold resize-none outline-none pl-4 rounded-[30px] border-lightgray border text-gray"
+              className="w-full py-[55.5px] max-sm:pt-10 max-sm:pb-5 mb-5 leading-6 placeholder:text-gray text-gray text-sm font-bold resize-none outline-none pl-4 rounded-[30px] border-lightgray border text-gray"
               placeholder="Details zur Veranstaltung" name="details" value={formData.details} onChange={handleChange} ></textarea>
             <button className="py-4 px-[115px] max-sm:px-0 max-sm:w-full text-center hover:scale-110 transition-all duration-700 rounded-[30px] mx-auto bg-custom-gradient sm:flex font-bold text-white">
               Event einsenden </button>
